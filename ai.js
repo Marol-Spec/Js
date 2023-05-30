3cc4b625af79aa2c23b117feff4d1dd8f0d6b590
Santos Beyah <67522377+Marol-Spec@users.noreply.github.com>
2023-05-30 13:47:28 +0800

    Create ai.js

---
+++ ai.js
@@ -0,0 +1,139 @@
+{
+  "data" : {
+    "type" : "adapty_inapps_apple_receipt_validation_result",
+    "id" : "8fd77397-1432-46a7-9549-6bab4cda09a6",
+    "attributes" : {
+      "app_id" : "1840ce65-2338-4474-9c29-e804dbeba43b",
+      "total_revenue_usd" : 0,
+      "customer_user_id" : null,
+      "subscriptions" : {
+        "com.chatgpt.yearly" : {
+          "vendor_transaction_id" : "190001599212196",
+          "billing_issue_detected_at" : null,
+          "is_lifetime" : false,
+          "store" : "app_store",
+          "vendor_product_id" : "com.chatgpt.yearly",
+          "vendor_original_transaction_id" : "190001599212196",
+          "will_renew" : true,
+          "renewed_at" : "2023-05-30T05:32:05.000000+0000",
+          "cancellation_reason" : null,
+          "active_promotional_offer_id" : null,
+          "active_promotional_offer_type" : null,
+          "unsubscribed_at" : null,
+          "is_active" : true,
+          "activated_at" : "2023-05-30T05:32:06.000000+0000",
+          "is_refund" : false,
+          "is_in_grace_period" : false,
+          "active_introductory_offer_type" : "free_trial",
+          "expires_at" : "2023-06-02T05:32:05.000000+0000",
+          "starts_at" : null,
+          "is_sandbox" : false
+        }
+      },
+      "promotional_offer_eligibility" : false,
+      "custom_attributes" : {
+
+      },
+      "profile_id" : "8fd77397-1432-46a7-9549-6bab4cda09a6",
+      "paid_access_levels" : {
+        "premium" : {
+          "id" : "premium",
+          "is_lifetime" : false,
+          "vendor_product_id" : "com.chatgpt.yearly",
+          "active_promotional_offer_type" : null,
+          "cancellation_reason" : null,
+          "billing_issue_detected_at" : null,
+          "unsubscribed_at" : null,
+          "expires_at" : "2023-06-02T05:32:05.000000+0000",
+          "will_renew" : true,
+          "is_active" : true,
+          "active_promotional_offer_id" : null,
+          "is_in_grace_period" : false,
+          "activated_at" : "2023-05-30T05:32:06.000000+0000",
+          "renewed_at" : "2023-05-30T05:32:05.000000+0000",
+          "is_refund" : false,
+          "active_introductory_offer_type" : "free_trial",
+          "store" : "app_store",
+          "starts_at" : null
+        }
+      },
+      "introductory_offer_eligibility" : false,
+      "apple_validation_result" : {
+        "environment" : "Production",
+        "receipt" : {
+          "receipt_type" : "Production",
+          "app_item_id" : 1660877567,
+          "receipt_creation_date" : "2023-05-30 05:32:40 Etc/GMT",
+          "bundle_id" : "com.eleven.chatgpt",
+          "original_purchase_date" : "2023-05-30 05:15:18 Etc/GMT",
+          "in_app" : [
+            {
+              "quantity" : "1",
+              "purchase_date_ms" : "1685424725000",
+              "expires_date" : "2023-06-02 05:32:05 Etc/GMT",
+              "expires_date_pst" : "2023-06-01 22:32:05 America/Los_Angeles",
+              "is_in_intro_offer_period" : "false",
+              "transaction_id" : "190001599212196",
+              "is_trial_period" : "true",
+              "original_transaction_id" : "190001599212196",
+              "purchase_date" : "2023-05-30 05:32:05 Etc/GMT",
+              "product_id" : "com.chatgpt.yearly",
+              "original_purchase_date_pst" : "2023-05-29 22:32:06 America/Los_Angeles",
+              "in_app_ownership_type" : "PURCHASED",
+              "original_purchase_date_ms" : "1685424726000",
+              "web_order_line_item_id" : "190000718385856",
+              "expires_date_ms" : "1685683925000",
+              "purchase_date_pst" : "2023-05-29 22:32:05 America/Los_Angeles",
+              "original_purchase_date" : "2023-05-30 05:32:06 Etc/GMT"
+            }
+          ],
+          "adam_id" : 1660877567,
+          "receipt_creation_date_pst" : "2023-05-29 22:32:40 America/Los_Angeles",
+          "request_date" : "2023-05-30 05:32:41 Etc/GMT",
+          "request_date_pst" : "2023-05-29 22:32:41 America/Los_Angeles",
+          "version_external_identifier" : 856692713,
+          "request_date_ms" : "1685424761276",
+          "original_purchase_date_pst" : "2023-05-29 22:15:18 America/Los_Angeles",
+          "application_version" : "13",
+          "original_purchase_date_ms" : "1685423718000",
+          "receipt_creation_date_ms" : "1685424760000",
+          "original_application_version" : "13",
+          "download_id" : 502477288351208136
+        },
+        "pending_renewal_info" : [
+          {
+            "product_id" : "com.chatgpt.yearly",
+            "original_transaction_id" : "190001599212196",
+            "auto_renew_product_id" : "com.chatgpt.yearly",
+            "auto_renew_status" : "1"
+          }
+        ],
+        "status" : 0,
+        "latest_receipt_info" : [
+          {
+            "quantity" : "1",
+            "purchase_date_ms" : "1685424725000",
+            "expires_date" : "2023-06-02 05:32:05 Etc/GMT",
+            "expires_date_pst" : "2023-06-01 22:32:05 America/Los_Angeles",
+            "is_in_intro_offer_period" : "false",
+            "transaction_id" : "190001599212196",
+            "is_trial_period" : "true",
+            "original_transaction_id" : "190001599212196",
+            "purchase_date" : "2023-05-30 05:32:05 Etc/GMT",
+            "product_id" : "com.chatgpt.yearly",
+            "original_purchase_date_pst" : "2023-05-29 22:32:06 America/Los_Angeles",
+            "in_app_ownership_type" : "PURCHASED",
+            "subscription_group_identifier" : "21101832",
+            "original_purchase_date_ms" : "1685424726000",
+            "web_order_line_item_id" : "190000718385856",
+            "expires_date_ms" : "1685683925000",
+            "purchase_date_pst" : "2023-05-29 22:32:05 America/Los_Angeles",
+            "original_purchase_date" : "2023-05-30 05:32:06 Etc/GMT"
+          }
+        ],
+        "latest_receipt" : "MIIUYAYJKoZIhvcNAQcCoIIUUTCCFE0CAQExCzAJBgUrDgMCGgUAMIIDngYJKoZIhvcNAQcBoIIDjwSCA4sxggOHMAoCARQCAQEEAgwAMAsCARkCAQEEAwIBAzAMAgEDAgEBBAQMAjEzMAwCAQ4CAQEEBAICAKowDAIBEwIBAQQEDAIxMzANAgEKAgEBBAUWAzEyKzANAgENAgEBBAUCAwJykTAOAgEBAgEBBAYCBGL++v8wDgIBCQIBAQQGAgRQMjYwMA4CAQsCAQEEBgIEB11hDDAOAgEQAgEBBAYCBDMQF+kwEgIBDwIBAQQKAggG+Shud+ASyDAUAgEAAgEBBAwMClByb2R1Y3Rpb24wGAIBBAIBAgQQpRd9gmr29FzBN+IHPQpSdTAcAgECAgEBBBQMEmNvbS5lbGV2ZW4uY2hhdGdwdDAcAgEFAgEBBBTcGiaSrzo3Pju2qdwF15RNK3HPTDAeAgEIAgEBBBYWFDIwMjMtMDUtMzBUMDU6MzI6NDBaMB4CAQwCAQEEFhYUMjAyMy0wNS0zMFQwNTozMjo0MVowHgIBEgIBAQQWFhQyMDIzLTA1LTMwVDA1OjE1OjE4WjAxAgEGAgEBBCnhGLOTZslcNj9SRBp4CHx57C6lrsCncJpZynBETpouf4WucQ0mcnvkETA+AgEHAgEBBDaO5Lw13Dqwdv/PpqnO0jPFdyJIKjHSsmaTuQulB2N2hV2eWGLTR3FFgofZZc7c0pgh0x3JWgUwggGPAgERAgEBBIIBhTGCAYEwCwICBq0CAQEEAgwAMAsCAgawAgEBBAIWADALAgIGsgIBAQQCDAAwCwICBrMCAQEEAgwAMAsCAga0AgEBBAIMADALAgIGtQIBAQQCDAAwCwICBrYCAQEEAgwAMAwCAgalAgEBBAMCAQEwDAICBqsCAQEEAwIBAzAMAgIGsQIBAQQDAgEBMAwCAga3AgEBBAMCAQAwDAICBroCAQEEAwIBADAQAgIGrgIBAQQHAgUBgCoR2zASAgIGrwIBAQQJAgcArM39U5LAMBoCAganAgEBBBEMDzE5MDAwMTU5OTIxMjE5NjAaAgIGqQIBAQQRDA8xOTAwMDE1OTkyMTIxOTYwHQICBqYCAQEEFAwSY29tLmNoYXRncHQueWVhcmx5MB8CAgaoAgEBBBYWFDIwMjMtMDUtMzBUMDU6MzI6MDVaMB8CAgaqAgEBBBYWFDIwMjMtMDUtMzBUMDU6MzI6MDZaMB8CAgasAgEBBBYWFDIwMjMtMDYtMDJUMDU6MzI6MDVaoIIO4jCCBcYwggSuoAMCAQICEC2rAxu91mVz0gcpeTxEl8QwDQYJKoZIhvcNAQEFBQAwdTELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAsMAkc3MUQwQgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTAeFw0yMjEyMDIyMTQ2MDRaFw0yMzExMTcyMDQwNTJaMIGJMTcwNQYDVQQDDC5NYWMgQXBwIFN0b3JlIGFuZCBpVHVuZXMgU3RvcmUgUmVjZWlwdCBTaWduaW5nMSwwKgYDVQQLDCNBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9uczETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDA3cautOi8bevBfbXOmFn2UFi2QtyV4xrF9c9kqn/SzGFM1hTjd4HEWTG3GcdNS6udJ6YcPlRyUCIePTAdSg5G5dgmKRVL4yCcrtXzJWPQmNRx+G6W846gCsUENek496v4O5TaB+VbOYX/nXlA9BoKrpVZmNMcXIpsBX2aHzRFwQTN1cmSpUYXBqykhfN3XB+F96NB5tsTEG9t8CHqrCamZj1eghXHXJsplk1+ik6OeLtXyTWUe7YAzhgKi3WVm+nDFD7BEDQEbbc8NzPfzRQ+YgzA3y9yu+1Kv+PIaQ1+lm0dTxA3btP8PRoGfWwBFMjEXzFqUvEzBchg48YDzSaBAgMBAAGjggI7MIICNzAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFF1CEGwbu8dSl05EvRMnuToSd4MrMHAGCCsGAQUFBwEBBGQwYjAtBggrBgEFBQcwAoYhaHR0cDovL2NlcnRzLmFwcGxlLmNvbS93d2RyZzcuZGVyMDEGCCsGAQUFBzABhiVodHRwOi8vb2NzcC5hcHBsZS5jb20vb2NzcDAzLXd3ZHJnNzAxMIIBHwYDVR0gBIIBFjCCARIwggEOBgoqhkiG92NkBQYBMIH/MDcGCCsGAQUFBwIBFitodHRwczovL3d3dy5hcHBsZS5jb20vY2VydGlmaWNhdGVhdXRob3JpdHkvMIHDBggrBgEFBQcCAjCBtgyBs1JlbGlhbmNlIG9uIHRoaXMgY2VydGlmaWNhdGUgYnkgYW55IHBhcnR5IGFzc3VtZXMgYWNjZXB0YW5jZSBvZiB0aGUgdGhlbiBhcHBsaWNhYmxlIHN0YW5kYXJkIHRlcm1zIGFuZCBjb25kaXRpb25zIG9mIHVzZSwgY2VydGlmaWNhdGUgcG9saWN5IGFuZCBjZXJ0aWZpY2F0aW9uIHByYWN0aWNlIHN0YXRlbWVudHMuMDAGA1UdHwQpMCcwJaAjoCGGH2h0dHA6Ly9jcmwuYXBwbGUuY29tL3d3ZHJnNy5jcmwwHQYDVR0OBBYEFLJFfcNEimtMSa9uUd4XyVFG7/s0MA4GA1UdDwEB/wQEAwIHgDAQBgoqhkiG92NkBgsBBAIFADANBgkqhkiG9w0BAQUFAAOCAQEAd4oC3aSykKWsn4edfl23vGkEoxr/ZHHT0comoYt48xUpPnDM61VwJJtTIgm4qzEslnj4is4Wi88oPhK14Xp0v0FMWQ1vgFYpRoGP7BWUD1D3mbeWf4Vzp5nsPiakVOzHvv9+JH/GxOZQFfFZG+T3hAcrFZSzlunYnoVdRHSuRdGo7/ml7h1WGVpt6isbohE0DTdAFODr8aPHdpVmDNvNXxtif+UqYPY5XY4tLqHFAblHXdHKW6VV6X6jexDzA6SCv8m0VaGIWCIF+v15a2FoEP+40e5e5KzMcoRsswIVK6o5r7AF5ldbD6QopimkS4d3naMQ32LYeWhg5/pOyshkyzCCBFUwggM9oAMCAQICFDQYWP8B/gY/jvGfH+k8AbTBRv/JMA0GCSqGSIb3DQEBBQUAMGIxCzAJBgNVBAYTAlVTMRMwEQYDVQQKEwpBcHBsZSBJbmMuMSYwJAYDVQQLEx1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEWMBQGA1UEAxMNQXBwbGUgUm9vdCBDQTAeFw0yMjExMTcyMDQwNTNaFw0yMzExMTcyMDQwNTJaMHUxCzAJBgNVBAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMQswCQYDVQQLDAJHNzFEMEIGA1UEAww7QXBwbGUgV29ybGR3aWRlIERldmVsb3BlciBSZWxhdGlvbnMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCsrtHTtoqxGyiVrd5RUUw/M+FOXK+z/ALSZU8q1HRojHUXZc8o5EgJmHFSMiwWTniOklZkqd2LzeLUxzuiEkU3AhliZC9/YcbTWSK/q/kUo+22npm6L/Gx3DBCT7a2ssZ0qmJWu+1ENg/R5SB0k1c6XZ7cAfx4b2kWNcNuAcKectRxNrF2CXq+DSqX8bBeCxsSrSurB99jLfWI6TISolVYQ3Y8PReAHynbsamfq5YFnRXc3dtOD+cTfForLgJB9u56arZzYPeXGRSLlTM4k9oAJTauVVp8n/n0YgQHdOkdp5VXI6wrJNpkTyhy6ZawCDyIGxRjQ9eJrpjB8i2O41ElAgMBAAGjge8wgewwEgYDVR0TAQH/BAgwBgEB/wIBADAfBgNVHSMEGDAWgBQr0GlHlHYJ/vRrjS5ApvdHTX8IXjBEBggrBgEFBQcBAQQ4MDYwNAYIKwYBBQUHMAGGKGh0dHA6Ly9vY3NwLmFwcGxlLmNvbS9vY3NwMDMtYXBwbGVyb290Y2EwLgYDVR0fBCcwJTAjoCGgH4YdaHR0cDovL2NybC5hcHBsZS5jb20vcm9vdC5jcmwwHQYDVR0OBBYEFF1CEGwbu8dSl05EvRMnuToSd4MrMA4GA1UdDwEB/wQEAwIBBjAQBgoqhkiG92NkBgIBBAIFADANBgkqhkiG9w0BAQUFAAOCAQEAUqMIKRNlt7Uf5jQD7fYYd7w9yie1cOzsbDNL9pkllAeeITMDavV9Ci4r3wipgt5Kf+HnC0sFuCeYSd3BDIbXgWSugpzERfHqjxwiMOOiJWFEif6FelbwcpJ8DERUJLe1pJ8m8DL5V51qeWxA7Q80BgZC/9gOMWVt5i4B2Qa/xcoNrkfUBReIPOmc5BlkbYqUrRHcAfbleK+t6HDXDV2BPkYqLK4kocfS4H2/HfU2a8XeqQqagLERXrJkfrPBV8zCbFmZt/Sw3THaSNZqge6yi1A1FubnXHFibrDyUeKobfgqy2hzxqbEGkNJAT6pqQCKhmyDiNJccFd62vh2zBnVsDCCBLswggOjoAMCAQICAQIwDQYJKoZIhvcNAQEFBQAwYjELMAkGA1UEBhMCVVMxEzARBgNVBAoTCkFwcGxlIEluYy4xJjAkBgNVBAsTHUFwcGxlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MRYwFAYDVQQDEw1BcHBsZSBSb290IENBMB4XDTA2MDQyNTIxNDAzNloXDTM1MDIwOTIxNDAzNlowYjELMAkGA1UEBhMCVVMxEzARBgNVBAoTCkFwcGxlIEluYy4xJjAkBgNVBAsTHUFwcGxlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MRYwFAYDVQQDEw1BcHBsZSBSb290IENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5JGpCR+R2x5HUOsF7V55hC3rNqJXTFXsixmJ3vlLbPUHqyIwAugYPvhQCdN/QaiY+dHKZpwkaxHQo7vkGyrDH5WeegykR4tb1BY3M8vED03OFGnRyRly9V0O1X9fm/IlA7pVj01dDfFkNSMVSxVZHbOU9/acns9QusFYUGePCLQg98usLCBvcLY/ATCMt0PPD5098ytJKBrI/s61uQ7ZXhzWyz21Oq30Dw4AkguxIRYudNU8DdtiFqujcZJHU1XBry9Bs/j743DN5qNMRX4fTGtQlkGJxHRiCxCDQYczioGxMFjsWgQyjGizjx3eZXP/Z15lvEnYdp8zFGWhd5TJLQIDAQABo4IBejCCAXYwDgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFCvQaUeUdgn+9GuNLkCm90dNfwheMB8GA1UdIwQYMBaAFCvQaUeUdgn+9GuNLkCm90dNfwheMIIBEQYDVR0gBIIBCDCCAQQwggEABgkqhkiG92NkBQEwgfIwKgYIKwYBBQUHAgEWHmh0dHBzOi8vd3d3LmFwcGxlLmNvbS9hcHBsZWNhLzCBwwYIKwYBBQUHAgIwgbYagbNSZWxpYW5jZSBvbiB0aGlzIGNlcnRpZmljYXRlIGJ5IGFueSBwYXJ0eSBhc3N1bWVzIGFjY2VwdGFuY2Ugb2YgdGhlIHRoZW4gYXBwbGljYWJsZSBzdGFuZGFyZCB0ZXJtcyBhbmQgY29uZGl0aW9ucyBvZiB1c2UsIGNlcnRpZmljYXRlIHBvbGljeSBhbmQgY2VydGlmaWNhdGlvbiBwcmFjdGljZSBzdGF0ZW1lbnRzLjANBgkqhkiG9w0BAQUFAAOCAQEAXDaZTC14t+2Mm9zzd5vydtJ3ME/BH4WDhRuZPUc38qmbQI4s1LGQEti+9HOb7tJkD8t5TzTYoj75eP9ryAfsfTmDi1Mg0zjEsb+aTwpr/yv8WacFCXwXQFYRHnTTt4sjO0ej1W8k4uvRt3DfD0XhJ8rxbXjt57UXF6jcfiI1yiXV2Q/Wa9SiJCMR96Gsj3OBYMYbWwkvkrL4REjwYDieFfU9JmcgijNq9w2Cz97roy/5U2pbZMBjM3f3OgcsVuvaDyEO2rpzGU+12TZ/wYdV2aeZuTJC+9jVcZ5+oVK3G72TQiQSKscPHbZNnF5jyEuAF1CqitXa5PzQCQc3sHV1ITGCAbEwggGtAgEBMIGJMHUxCzAJBgNVBAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMQswCQYDVQQLDAJHNzFEMEIGA1UEAww7QXBwbGUgV29ybGR3aWRlIERldmVsb3BlciBSZWxhdGlvbnMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkCEC2rAxu91mVz0gcpeTxEl8QwCQYFKw4DAhoFADANBgkqhkiG9w0BAQEFAASCAQB4DUCwQ9K3RhQcVkdI8CHzKaAGpx50yz5ESqx2M/yi27YrAvcUeQA6Cw13F8GH76pEB6aTGlSqhUXo59/jRsmqzFoXa0ggH++TnjGAxXPwk5eQopCbByEConPwaClYZQXStJRTiYPA9/dn68D02mXr9r+bSDYRC1I+OqyEIGSGKZjov8vD8CiOAaw8eQQIYrAyI7F4MGU5hPG7EXi+J8OEjYIxq56uejuOgl9SooriWaUmfaA7G2LzxB3t6j2Or7ToGJB9kj64RVqvx7lSWRYQ6Ex0gOGH/QH5kmSIc/fYWZUVTzCDEqMH4uk1aqCpB4YLkiMVSJHfWQj1fjx8bRLu"
+      },
+      "non_subscriptions" : null
+    }
+  }
+}
